3c773b6b352163c435b814e4a84721839ab06a64
David Baldwynn <whitef0x0@users.noreply.github.com>
2015-10-13 12:32:15 -0700

    Update registration.js

--- public/js/registration.js
+++ public/js/registration.js
@@ -3,67 +3,114 @@ $(function() {
  
  Parse.initialize("QWlOvmgg8hIVbJyA2BDF9558rxDNRyJIk1dHZtIv", "3WAaTWRerKvgl22YTw8eOPK9GHCRTBR4gVvOtGog");
   
-  // handler for form submission
-  $('#signup').on("click", function(event) {
-    var $form = $('form');
-    
-    var $target = $($form.attr('data-target'));
+  // handler for startup form submission
+  $('#startup_form').on("click", function(event) {
+    var $form = $('#startup_form');
+
     // Log the user out in case they're still logged in
     Parse.User.logOut();
     
     var data = convertFormToJSON($form);
-    data.password = Math.random().toString(36).substring(2);
-    data.username = data.email;
+    var user_data = {};
+    user_data.password = Math.random().toString(36).substring(2);
+    user_data.username = data.email;
     
     var user = new Parse.User();
     user.signUp(data, {
       success: function(user) {
-        $("#prereg-btn").show();
-        $("#sponsor-btn").show();
-        $("#signup").hide();
-        $("#cancel").hide();
         
-        $modal = $('#prereg-modal');
+        $modal = $('.signup.modal');
+        $modal.modal('hide');
         
-        $modal.find('.modal-title').text('Submission Successful');
-        $modal.find('.modal-body p').text('Thanks for your showing your interest! You\'ll be the first to know when registration opens for nwHacks 2016.');
-        $modal.modal('show');
+        var startup = new Startup();
+        startup.set("user", user);
+  
+        var startupData.company = {
+          name: data.name,
+          url: data.url,
+          description: data.description
+        };
         
-        //window.alert("Thanks for your showing your interest! You'll be the first to know when registration opens for nwHacks.");
-        $("form").find("input[type=text]").val("");
-        window.setTimeout( $('form').hide(), 500);
+        var startupData.candidates = {
+          roles: data.roles,
+          locations: data.locations
+        };
+        
+    
+        startup.save(startupData, {
+          success: function(startup){
+            console.log('startup successfully saved');
+          },
+          error: function(startup, error){
+            console.log('Startup could not be saved. Error: '+error.message);
+          }
+        });
 
       },
       error: function(user, error) {
-        //$("#signup").show();
-        $modal = $('#prereg-modal');
-        
-        $modal.find('.modal-title').text('Submission Error');
-        $modal.find('.modal-body p').text(error.message);
-        $modal.modal('show');
-        //window.alert("There was an error with your application: " + error.message);
+        // $modal = $('.signup.modal');
+        // $modal.find('.modal-title').text('Submission Error');
+        // $modal.find('.modal-body p').text(error.message);
+        console.error('Submission Error: '+error.messasge);
+        // $modal.modal('show');
+        // window.alert("There was an error with your application: " + error.message);
       }
     });
     event.preventDefault();
 
   });
   
+   // Our basic Startup model
+  var Todo = Parse.Object.extend("Startup", {
+    // Default attributes for the startup.
+    defaults: {
+      company: {
+        name: "",
+        url: "",
+        description: "",
+        size: "",
+      },
+      candidates:{
+        roles: [],
+        locations: [],
+        gradYears: [],
+      },
+      paidCurrentBatch: false,
+      activated: false,
+    },
+
+    // Ensure that each todo created is linked to a parse user
+    initialize: function() {
+      if (!this.get("user")) {
+        this.set({"user": Parse.User.current()});
+      }
+    },
+
+    // Toggle the `done` state of this todo item.
+    toggleActivation: function() {
+      this.save({activated: !this.get("activated")});
+    }
+  });
+
+  // This is the transient application state, not persisted on Parse
+  var AppState = Parse.Object.extend("AppState", {
+    defaults: {
+      filter: "all"
+    }
+  });
+  
   // The Application
   // ---------------
 
   // The main view that lets a user manage their todo items
-  var ManageTodosView = Parse.View.extend({
-
-    // Our template for the line of statistics at the bottom of the app.
-    statsTemplate: _.template($('#stats-template').html()),
+  var StartupDashboardView = Parse.View.extend({
 
     // Delegated events for creating new items, and clearing completed ones.
     events: {
-      "keypress #new-todo":  "createOnEnter",
-      "click #clear-completed": "clearCompleted",
-      "click #toggle-all": "toggleAllComplete",
+      "keypress #pay-batch":  "payBatch",
+      "keypress #save-batch":  "updateBatchSettings",
+      "click #save-profile": "saveProfile",
       "click .log-out": "logOut",
-      "click ul#filters a": "selectFilter"
     },
 
     el: ".content",
@@ -74,27 +121,22 @@ $(function() {
     initialize: function() {
       var self = this;
 
-      _.bindAll(this, 'addOne', 'addAll', 'addSome', 'render', 'toggleAllComplete', 'logOut', 'createOnEnter');
+      _.bindAll(this, 'logOut');
 
       // Main todo management template
-      this.$el.html(_.template($("#manage-todos-template").html()));
+      this.$el.html(_.template($("#dashboard-template").html()));
+
       
-      this.input = this.$("#new-todo");
-      this.allCheckbox = this.$("#toggle-all")[0];
-
       // Create our collection of Todos
-      this.todos = new TodoList;
-
+      this.startup = Parse.Object.extend("Startup");
+      
       // Setup the query for the collection to look for todos from the current user
-      this.todos.query = new Parse.Query(Todo);
-      this.todos.query.equalTo("user", Parse.User.current());
+      this.startup.query = new Parse.Query(Startup);
+      this.startup.query.equalTo("user", Parse.User.current());
         
-      this.todos.bind('add',     this.addOne);
-      this.todos.bind('reset',   this.addAll);
-      this.todos.bind('all',     this.render);
 
       // Fetch all the todo items for this user
-      this.todos.fetch();
+      this.startup.fetch();
 
       state.on("change", this.filter, this);
     },
@@ -107,104 +149,11 @@ $(function() {
       delete this;
     },
 
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
-    render: function() {
-      var done = this.todos.done().length;
-      var remaining = this.todos.remaining().length;
-
-      this.$('#todo-stats').html(this.statsTemplate({
-        total:      this.todos.length,
-        done:       done,
-        remaining:  remaining
-      }));
-
-      this.delegateEvents();
-
-      this.allCheckbox.checked = !remaining;
-    },
-
-    // Filters the list based on which type of filter is selected
-    selectFilter: function(e) {
-      var el = $(e.target);
-      var filterValue = el.attr("id");
-      state.set({filter: filterValue});
-      Parse.history.navigate(filterValue);
-    },
-
-    filter: function() {
-      var filterValue = state.get("filter");
-      this.$("ul#filters a").removeClass("selected");
-      this.$("ul#filters a#" + filterValue).addClass("selected");
-      if (filterValue === "all") {
-        this.addAll();
-      } else if (filterValue === "completed") {
-        this.addSome(function(item) { return item.get('done') });
-      } else {
-        this.addSome(function(item) { return !item.get('done') });
-      }
-    },
-
-    // Resets the filters to display all todos
-    resetFilters: function() {
-      this.$("ul#filters a").removeClass("selected");
-      this.$("ul#filters a#all").addClass("selected");
-      this.addAll();
-    },
-
-    // Add a single todo item to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
-    addOne: function(todo) {
-      var view = new TodoView({model: todo});
-      this.$("#todo-list").append(view.render().el);
-    },
-
-    // Add all items in the Todos collection at once.
-    addAll: function(collection, filter) {
-      this.$("#todo-list").html("");
-      this.todos.each(this.addOne);
-    },
-
-    // Only adds some todos, based on a filtering function that is passed in
-    addSome: function(filter) {
-      var self = this;
-      this.$("#todo-list").html("");
-      this.todos.chain().filter(filter).each(function(item) { self.addOne(item) });
-    },
-
-    // If you hit return in the main input field, create new Todo model
-    createOnEnter: function(e) {
-      var self = this;
-      if (e.keyCode != 13) return;
-
-      this.todos.create({
-        content: this.input.val(),
-        order:   this.todos.nextOrder(),
-        done:    false,
-        user:    Parse.User.current(),
-        ACL:     new Parse.ACL(Parse.User.current())
-      });
-
-      this.input.val('');
-      this.resetFilters();
-    },
-
-    // Clear all done todo items, destroying their models.
-    clearCompleted: function() {
-      _.each(this.todos.done(), function(todo){ todo.destroy(); });
-      return false;
-    },
-
-    toggleAllComplete: function () {
-      var done = this.allCheckbox.checked;
-      this.todos.each(function (todo) { todo.save({'done': done}); });
-    }
   });
 
   var LogInView = Parse.View.extend({
     events: {
       "submit form.login-form": "logIn",
-      "submit form.signup-form": "signUp"
     },
 
     el: ".content",
@@ -236,30 +185,7 @@ $(function() {
 
       return false;
     },
-
-    signUp: function(e) {
-      var self = this;
-      var username = this.$("#signup-username").val();
-      var password = this.$("#signup-password").val();
-      
-      Parse.User.signUp(username, password, { ACL: new Parse.ACL() }, {
-        success: function(user) {
-          new ManageTodosView();
-          self.undelegateEvents();
-          delete self;
-        },
-
-        error: function(user, error) {
-          self.$(".signup-form .error").html(_.escape(error.message)).show();
-          self.$(".signup-form button").removeAttr("disabled");
-        }
-      });
-
-      this.$(".signup-form button").attr("disabled", "disabled");
-
-      return false;
-    },
-
+    
     render: function() {
       this.$el.html(_.template($("#login-template").html()));
       this.delegateEvents();
@@ -278,7 +204,7 @@ $(function() {
 
     render: function() {
       if (Parse.User.current()) {
-        new ManageTodosView();
+        new StartupDashboardView();
       } else {
         new LogInView();
       }
